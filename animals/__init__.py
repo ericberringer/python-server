107be699657d3431458a574aacd6756ae3a0d128
ericberringer <eric.berringer@gmail.com>
2021-04-09 14:11:04 -0500

    chapter 5 deletes work, refactored post and it still works.

--- animals/__init__.py
+++ animals/__init__.py
@@ -1,5 +1,6 @@
 from .request import get_all_animals
 from .request import get_single_animal
 from .request import create_animal
+from .request import delete_animal
 # how folders are turned into modules
 #  import get all animals from the request.py file
