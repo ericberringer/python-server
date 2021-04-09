af278b3891475907938187c6e9d3fc60e57aca98
ericberringer <eric.berringer@gmail.com>
2021-04-09 15:59:00 -0500

    Chapter 6 put methods are working on everything.

--- animals/__init__.py
+++ animals/__init__.py
@@ -2,5 +2,6 @@ from .request import get_all_animals
 from .request import get_single_animal
 from .request import create_animal
 from .request import delete_animal
+from .request import update_animal
 # how folders are turned into modules
 #  import get all animals from the request.py file
